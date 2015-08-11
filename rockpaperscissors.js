2b92a5824c7f35b36810413f1d2065d26ed9ba2d
Kyle Smith <kylesmith@Kyles-MacBook-Pro.local>
2015-08-11 15:48:02 -0700

    winner is decided

---
+++ rockpaperscissors.js
@@ -0,0 +1,76 @@
+////////////////////////////////////////////////
+/*   Provided Code - Please Don't Edit   */
+////////////////////////////////////////////////
+console.log("Begining");
+'use strict';
+
+function getInput() {
+    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
+    return prompt();
+}
+function randomPlay() {
+    var randomNumber = Math.random();
+    if (randomNumber < 0.33) {
+        return "rock";
+    } else if (randomNumber < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+
+function getPlayerMove(move) 
+{
+    move = move || getInput();
+    return move;
+}
+
+function getComputerMove(move) {
+    move = move || randomPlay();
+    return move;
+}
+
+function getWinner(playerMove,computerMove) {
+    var winner;
+    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
+    var winCond1 = playerMove === 'rock' && computerMove === 'scissors';
+    var winCond2 = playerMove === 'scissors' && computerMove === 'paper';
+    var winCond3 = playerMove === 'paper' && computerMove === 'rock';
+    if (playerMove ===computerMove)
+    {
+        winner = 'tie';
+    }
+    else if (winCond1 || winCond2 || winCond3)
+    {
+        winner = 'player';
+    }
+    else
+    {
+        winner = 'computer';
+    }
+    console.log("Your move was " + playerMove + ", computer's move was " + computerMove);
+    if (winner != 'tie')
+    {
+        console.log(winner + " is the winner!");     
+    }
+    else
+    {
+        console.log("Tie!!");
+    }
+
+    return winner;
+}
+
+function playToFive() {
+    console.log("Let's play Rock, Paper, Scissors");
+    var playerWins = 0;
+    var computerWins = 0;
+    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
+    /* YOUR CODE HERE */
+    return [playerWins, computerWins];
+}
+
+var pMove = getPlayerMove();
+var cMove = getComputerMove();
+getWinner(pMove,cMove);
