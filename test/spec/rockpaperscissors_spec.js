74de327e17a6255990c26c0ea3959d3ecdf84ce7
Kyle Smith <kylesmith@Kyles-MacBook-Pro.local>
2015-08-16 20:49:59 -0700

    spec file

--- test/spec/rockpaperscissors_spec.js
+++ test/spec/rockpaperscissors_spec.js
@@ -1,4 +1,4 @@
-xdescribe("#getPlayerMove", function() {
+describe("#getPlayerMove", function() {
   describe("when a move is given as a parameter", function() {
     it("should return that move", function(){
       expect(getPlayerMove('rock')).toEqual('rock');
@@ -8,7 +8,7 @@ xdescribe("#getPlayerMove", function() {
   });
 });
 
-xdescribe("#getComputerMove", function() {
+describe("#getComputerMove", function() {
   describe("when a move is given as a parameter", function() {
     it("should return that move", function(){
       expect(getComputerMove('rock')).toEqual('rock');
@@ -18,7 +18,7 @@ xdescribe("#getComputerMove", function() {
   });
 });
 
-xdescribe("#getWinner", function() {
+describe("#getWinner", function() {
   var playerMove;
   describe("when the player picks rock", function() {
     beforeEach(function(){
